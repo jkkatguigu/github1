6a460e9280e3766b85abf6045c4835ecaad0a410
敬开科 <914342400@qq.com>
2020-09-26 22:22:35 +0800

    演示合并分支产生冲突

--- src/main/java/com/atguigu/bean/love.java
+++ src/main/java/com/atguigu/bean/love.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class love {
     public static void main(String[] args) {
         System.out.println("今天是个好日子，创建分子");
+        System.out.println(" 宰猪，大家辛苦了" );
     }
 }
