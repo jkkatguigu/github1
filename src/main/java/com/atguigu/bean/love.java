88ddb3d4c5def2d7b886d0e31c3e6ab719cbf19f
敬开科 <914342400@qq.com>
2020-09-26 23:18:46 +0800

    测试push

--- src/main/java/com/atguigu/bean/love.java
+++ src/main/java/com/atguigu/bean/love.java
@@ -7,5 +7,6 @@ public class love {
         System.out.println("今天是个好日子，创建分子");
         System.out.println("宰猪");
         System.out.println(" 宰猪，大家辛苦了" );
+        System.out.println(" 我是李四" );
     }
 }
