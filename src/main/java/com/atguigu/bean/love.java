586552e864249e472e58a2cc833064dbe376c54f
敬开科 <914342400@qq.com>
2020-09-26 22:17:14 +0800

    演示git，创建分支

--- src/main/java/com/atguigu/bean/love.java
+++ src/main/java/com/atguigu/bean/love.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 
 public class love {
     public static void main(String[] args) {
-        System.out.println("今天是个好日子");
+        System.out.println("今天是个好日子，创建分子");
     }
 }
